4987a78a4b0610ad9d23011de004c9f46c6f3774
Rami Al Kawadri <ramiak@netcompany.com>
2026-08-19 07:32:23 +0200

    Use filescoped namespace and remove roslyn

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -2,23 +2,19 @@
 
 return {
     {
-        "khoido2003/roslyn-filewatch.nvim",
-        build = "nvim -l build.lua --",
-        config = function()
-            require("roslyn_filewatch").setup()
-        end,
-    },
-    {
         "DestopLine/boilersharp.nvim",
         ---@type boilersharp.Config
-        opts = { auto_install_xml_parser = false },
+        opts = {
+            namespace = {
+                use_file_scoped = "always",
+            },
+        },
     },
     {
         "seblyng/roslyn.nvim",
         ---@module "roslyn.config"
         ---@type RoslynNvimConfig
         opts = {
-            filewatching = "off",
             extensions = {
                 razor = {
                     enabled = false,
